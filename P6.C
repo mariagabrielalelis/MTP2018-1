a48bc1c64b989d93de707a9c421597f400e36727
Maria Gabriela <38012576+mariagabrielalelis@users.noreply.github.com>
2018-05-25 23:19:05 -0300

    Update P6.C

--- P6.C
+++ P6.C
@@ -2,32 +2,46 @@
 
 
 #include <stdio.h>
-int main(){
-   int s=10;
-   int i;
-   float med;
-   float nume[20];
-   float media(float [], int);
-   do{
-   printf("Digite os numeros inteiros desejados para que seja calculada a media, quando desejar parar digite o numero zero");
-      for(i = 0;; i++)
-      {
-      printf("\n\n%d numero:", i+1);
-      scanf("%f", &nume[i]);
-      if(nume[i] == 0){
-         goto media;
-      }
-   }
-   media:
-   med = media(nume, i);
-   printf("A media dos %d numeros digitados por voce e': %f.\n", i, med);
-   }while(s<11);
+
+float media(int soma, float n){
+	float mediaDivisao;
+	mediaDivisao = soma/n;
+	return mediaDivisao;
 }
-float media(float nume[], int num){
-   int i;
-   float soma = 0.0;
-   for(i = 0; i <= num-1; i++){
-      soma += nume[i];
-   }
-   return(soma/num);
-}
+
+int numerovetor(int vet[20], int n) {
+   	int i, soma=0;
+   	for (i=0;i<n;i++)
+    {
+    	soma+=vet[i];
+	}
+      return (soma);
+    }
+
+int main()
+{
+
+	int i, num[20], soma, n;
+
+	do
+	{
+		printf("Quantidade de numeros para fazer a media:");
+		scanf("%d",&n);
+    }
+    while(n<5||n>20);
+
+	for(i=0; i<20; i++){num[i] = 0;}
+
+	for(i=0; i<n; i++)
+	{
+    printf("\n %d numero: ", i+1);
+    scanf("%d", &num[i]);
+    getchar();
+	}
+	soma = (numerovetor(num, i));
+
+	printf("\nMedia - %f", media(soma, n));
+
+	return 0;
+	}
+
