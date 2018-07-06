c0ce4ffd767015e2345929f2309a997abe8ecbe8
Maria Gabriela <38012576+mariagabrielalelis@users.noreply.github.com>
2018-07-06 11:53:13 -0300

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,28 +1,39 @@
-//Maria Gabriela de OLiveira Lelis 11621EBI013
-
+//Maria Gabriela de Oliveira Lelis
+//11621EBI013
 
 #include <stdio.h>
-int main() {
 
-  int estado = 0;
-  int numero = 0;
-  int i = 0;
-  char binario[256];
-  printf ("Insira o número binário desejado:");
-  scanf("%s", binario);
-  while(binario[i] != ’\0’){
-  if (binario[i] == '1') {
-      numero = numero * 2 + 1;
-      ++i;
+int main()
+{
+    int estado = 0, i = 0;
+    char bin[256];
+    printf("\nInsira um numero em binario desejado:");
+    scanf("%s", bin);
+
+	while(bin[i] != '\0')
+    {
+        if(bin[i] == '0')
+        {
+            if(estado == 0);
+            else if(estado == 1) estado = 2;
+            else if(estado == 2) estado = 1;
+            i++;
+        }
+        else if(bin[i] == '1')
+        {
+            if(estado == 0) estado = 1;
+            else if(estado == 1) estado = 0;
+            else if(estado == 2);
+            i++;
+        }
+        else
+        {
+            printf("\nNumero inserido nao eh um numero binario");
+            return 0;
+        }
     }
-    else {
-      numero = numero * 2;
-      ++i;
-    }
-  }
-  if (numero % 3 == 0 )
-    printf ("e' divisivel por 3");
-  else
-   printf ("nao e' divisivel por 3");
-return 0;
+
+    if(estado == 0) printf("\n O numero %s e multiplo de 3", bin);
+    else printf("\n O numero binario %s nao e multiplo de 3", bin);
+    return 0;
 }
